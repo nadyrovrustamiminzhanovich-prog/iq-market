1796de05bb1655a1d690425c00ccf9aa0f5aa39f
Твое_Имя <___@example.com>
2026-06-15 00:44:53 +0500

    refactor: clean up obsolete files, fix hosting site, and fix debugPrint compile error in telegram_bot_service

--- lib/services/telegram_bot_service.dart
+++ lib/services/telegram_bot_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:math';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
